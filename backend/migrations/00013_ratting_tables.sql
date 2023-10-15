01a8bf39d8a0ca86eb4f09710bdf5cd6c9466df5
dinhquangkhuong <21021508@nvu.edu.vn>
2023-10-15 13:24:49 +0700

    change database

--- backend/migrations/00013_ratting_tables.sql
+++ backend/migrations/00013_ratting_tables.sql
@@ -2,7 +2,6 @@ create table ratting(
     user_email varchar(64) not null,
     book_id varchar(64) not null,
     ratting float not null,
-    description text,
     
     foreign key(user_email) references user(email),
     foreign key(book_id) references book(id)
