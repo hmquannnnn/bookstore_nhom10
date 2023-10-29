7c261bda6afe38f86f1b28cc202729c8dc53a8bc
dinhquangkhuong <21021508@nvu.edu.vn>
2023-10-29 09:09:58 +0700

    save

--- backend/src/header/mod.rs
+++ backend/src/header/mod.rs
@@ -1,28 +1,34 @@
 use actix_web::{dev::Payload, FromRequest, HttpRequest};
 use std::{future::Future, pin::Pin};
 
+#[derive(Clone)]
 pub struct AuthHeader(pub String);
 
-//
+// .ok_or(actix_web::error::ErrorUnauthorized("unknown user"));
 impl FromRequest for AuthHeader {
     type Error = actix_web::error::Error;
     type Future = Pin<Box<dyn Future<Output = Result<AuthHeader, Self::Error>>>>;
 
     fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
-        let req = req.headers().clone();
-        let header_value = req.into_iter().find(|header| header.0.as_str() == "auth");
-        Pin::from(Box::new(async move {
-            match header_value {
-                Some(header) => {
-                    let header_value = header
-                        .1
-                        .to_str()
-                        .map_err(|_| actix_web::error::ErrorUnauthorized("unknown user"))?
-                        .to_string();
-                    Ok(AuthHeader(header_value))
-                }
-                None => Err(actix_web::error::ErrorUnauthorized("unknown user")),
-            }
-        }))
+        let req = req.headers();
+        let auth_header = req.into_iter().find(|header| header.0.as_str() == "auth");
+        let header_value = auth_header
+            .map(|value| {
+                value
+                    .1
+                    .to_str()
+                    .map_err(|_| actix_web::error::ErrorUnauthorized("unknown user"))
+            })
+            .ok_or(actix_web::error::ErrorUnauthorized("unknown user"));
+
+        // combime two error
+        let error_handler = |result_value| {
+            let result_value = result_value?;
+            let header_value = result_value?;
+            Ok(header_value)
+        };
+
+        let header_value = error_handler(header_value).map(|value| AuthHeader(value.to_string()));
+        Pin::from(Box::new(async move { header_value }))
     }
 }
