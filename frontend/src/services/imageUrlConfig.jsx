2fa27d46f084642c19a43d7c54a84a77c1a249ee
dinhquangkhuong <21021508@nvu.edu.vn>
2023-12-29 23:04:25 +0700

    fix image url

--- frontend/src/services/imageUrlConfig.jsx
+++ frontend/src/services/imageUrlConfig.jsx
@@ -1,15 +1,10 @@
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const imageUrlConfig = (originalURL) => {
-    try {
-        if (originalURL.includes('http://localhost:8080/api')) {
-            console.log(originalURL);
-            return originalURL.replace('http://localhost:8080/api', baseURL);
-        }
-        return originalURL;
-    } catch (error) {
-        return "";
+    if (originalURL == null) {
+        return baseURL + "/image?id=" + originalURL.split('=').pop();
     }
+    return "";
 }
 
 export default imageUrlConfig;
